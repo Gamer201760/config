069f85331ba266e47d2dccc96e556661195112b0
Azamat <azamat201760@yandex.ru>
2024-07-14 17:03:33 +0500

    [nvim] add mapping for buffer

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -5,16 +5,25 @@ vim.g.maplocalleader = "\\"
 
 local opts = { noremap = true, silent = true }
 
+vim.keymap.set('i', 'jj', '<esc>', opts)
 
+-- save & exit
 vim.keymap.set({ 'n' }, '<Leader>w', '<cmd>w<cr>', opts)
 vim.keymap.set({ 'n' }, '<Leader>q', '<cmd>q<cr>', opts)
 
+-- features
 vim.keymap.set({ 'n' }, '<Leader>j', '<cmd>Neotree float<cr>', opts)
 vim.keymap.set({ 'n' }, "<Leader>'", '<cmd>TSJToggle<cr>', opts)
+
+-- comment string
 vim.keymap.set({ 'n', 'v' }, "<Leader>/", '<cmd>normal gcc<cr>', opts)
 
+-- Telescope fast nav in project
 vim.keymap.set({ 'n' }, "<Leader>f", '<cmd>Telescope lsp_document_symbols<cr>', opts)
 vim.keymap.set({ 'n' }, "<Leader>F", '<cmd>Telescope lsp_dynamic_workspace_symbols<cr>', opts)
 vim.keymap.set({ 'n' }, "<Leader>d", '<cmd>Telescope diagnostics<cr>', opts)
 
-vim.keymap.set('i', 'jj', '<esc>', opts)
+-- Buffer nav
+vim.keymap.set({ 'n', 'i', 'v' }, "<M-Tab>", '<cmd>bnext<cr>', opts)
+vim.keymap.set({ 'n', 'i', 'v' }, "<M-p>", '<cmd>bprevious<cr>', opts)
+vim.keymap.set({ 'n', 'i', 'v' }, "<M-q>", '<cmd>bd<cr>', opts)
--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -1,6 +1,6 @@
 return {
     { "hrsh7th/nvim-cmp",                 event = { "InsertEnter", "CmdlineEnter" }, dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer" } },
-    { 'echasnovski/mini.nvim' },
+    { 'echasnovski/mini.nvim',            version = false },
     { "williamboman/mason.nvim" },
     { "williamboman/mason-lspconfig.nvim" },
     { "neovim/nvim-lspconfig" },
