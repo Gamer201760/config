b0bfa7e1d8ffc2abc892a26e2d8d767a0cdc4182
Azamat <azamat201760@yandex.ru>
2025-12-18 00:27:23 +0300

    [fix] debug

--- nvim/lua/config/debug.lua
+++ nvim/lua/config/debug.lua
@@ -44,9 +44,9 @@ require('dapui').setup({
 }
 )
 
-vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
-vim.keymap.set('n', '<F6>', function() require('dapui').toggle() end)
-vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
-vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
-vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
-vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<F5>', require('dap').continue)
+vim.keymap.set('n', '<F6>', require('dapui').toggle)
+vim.keymap.set('n', '<F10>', require('dap').step_over)
+vim.keymap.set('n', '<F11>', require('dap').step_into)
+vim.keymap.set('n', '<F12>', require('dap').step_out)
+vim.keymap.set('n', '<Leader>b', require('dap').toggle_breakpoint)
